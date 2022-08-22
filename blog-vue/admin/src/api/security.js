51532b7d2bcc62975215aea015e691b652683fa9
世纪末的架构师 <1072876976@qq.com>
2022-08-23 00:19:08 +0800

    feat(项目前端): 完成前端角色管理编辑角色以及删除角色功能
    1. 封装编辑角色、删除角色请求
    2. 完成编辑角色以及删除角色功能以及数据渲染

--- blog-vue/admin/src/api/security.js
+++ blog-vue/admin/src/api/security.js
@@ -14,3 +14,26 @@ export const getAllRoles = () => {
     method: "GET",
   });
 };
+
+export const insertRole = (data) => {
+  return request({
+    url: "/admin/insertRole",
+    method: "POST",
+    data,
+  });
+};
+
+export const updateRole = (data) => {
+  return request({
+    url: "/admin/updateRole",
+    method: "PUT",
+    data,
+  });
+};
+
+export const deleteRole = (id) => {
+  return request({
+    url: `/admin/deleteRole/${id}`,
+    method: "DELETE",
+  });
+};
--- blog-vue/admin/src/views/security/Roles.vue
+++ blog-vue/admin/src/views/security/Roles.vue
@@ -49,20 +49,21 @@
                 @click="handleUpdate(scope.row)"
                 type="text"
                 size="small"
+                v-if="userRole == 'ROLE_admin'"
                 >编辑角色
               </el-button>
               <el-button
                 @click="handleUpdate(scope.row)"
                 type="text"
                 size="small"
-                v-if="scope.row.id != 1"
+                v-if="scope.row.roleName != 'admin' && userRole == 'ROLE_admin'"
                 >分配权限
               </el-button>
               <el-button
                 type="text"
                 size="small"
-                v-if="scope.row.id != 1"
-                @click="handleDelete(scope.row.id)"
+                v-if="scope.row.roleName != 'admin' && userRole == 'ROLE_admin'"
+                @click="handleDelete(scope.row)"
                 >删除角色
               </el-button>
             </template>
@@ -70,19 +71,79 @@
         </el-table>
       </div>
     </el-card>
+
+    <el-dialog
+      :title="dialogTitle"
+      width="500px"
+      :visible="dialogShow"
+      :before-close="handleClose"
+    >
+      <el-form>
+        <el-form-item label="角色名称" label-width="80px">
+          <el-input
+            placeholder="请输入角色名称"
+            v-model="role.roleNameZh"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="权限字符" label-width="80px">
+          <el-input v-model="role.roleName" placeholder="请输入权限字符">
+            <template slot="prepend">ROLE_</template>
+          </el-input>
+        </el-form-item>
+        <el-form-item label="创建时间" label-width="80px">
+          <el-input
+            placeholder="当前分类的创建时间"
+            :value="dateFormat(role.createTime)"
+            disabled
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="更新时间" label-width="80px">
+          <el-input
+            placeholder="当前分类的更新时间"
+            :value="dateFormat(role.updateTime)"
+            disabled
+          ></el-input>
+        </el-form-item>
+      </el-form>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确定</el-button>
+        <el-button @click="handleClose">取消</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { getAllRoles } from "@/api/security";
+import dayjs from "dayjs";
+import {
+  getAllRoles,
+  insertRole,
+  updateRole,
+  deleteRole,
+} from "@/api/security";
 
 export default {
   name: "Roles",
 
   data() {
     return {
+      // 当前登录的角色
+      userRole: this.$store.getters.userInfo.roles[0].roleName,
       // 角色数据
       roleList: [],
+      // 对话框是否显示
+      dialogShow: false,
+      // 对话框标题
+      dialogTitle: "",
+      // 单个角色数据
+      role: {
+        id: undefined,
+        roleName: "",
+        roleNameZh: "",
+        createTime: "",
+        updateTime: "",
+      },
     };
   },
 
@@ -100,13 +161,66 @@ export default {
 
     // 点击添加按钮
     handleAdd() {
-      console.log("点击了添加按钮");
+      this.dialogTitle = "新增角色";
+      this.role.createTime = this.dateFormat(new Date());
+      this.role.updateTime = this.dateFormat(new Date());
+      this.dialogShow = true;
     },
-    handleDelete() {
-      console.log("点击了删除按钮");
+    handleDelete(row) {
+      console.log(row);
+      this.$confirm("确定删除 " + row.roleNameZh + " 吗？", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          // 执行删除操作
+          deleteRole(row.id).then(() => {
+            // 重新获取表格数据
+            this.getTableData();
+          });
+        })
+        .catch(() => {});
     },
-    handleUpdate() {
-      console.log("点击了编辑按钮");
+    handleUpdate(row) {
+      this.dialogTitle = "更新角色";
+      this.role = row;
+      this.dialogShow = true;
+    },
+    // 关闭对话框
+    handleClose() {
+      this.dialogShow = false;
+      this.reset();
+    },
+    // 点击提交按钮
+    submitForm() {
+      this.dialogShow = false;
+      this.role.roleName = "ROLE_" + this.role.roleName;
+      // 如果存在ID则说明为更新操作
+      if (this.role.id) {
+        updateRole(this.role).then(() => {
+          this.getTableData();
+        });
+      } else {
+        // 不存在ID则说明为新增操作
+        insertRole(this.role).then(() => {
+          this.getTableData();
+        });
+      }
+    },
+
+    // 刷新表单数据
+    reset() {
+      // 修改表单标题
+      this.dialogTitle = "";
+      // 重置表单对象
+      this.role = {};
+    },
+
+    // 格式化时间
+    dateFormat(date) {
+      // 使用 dayjs 处理时间
+      return dayjs(date).format("YYYY-MM-DD HH:mm:ss");
     },
   },
 };
