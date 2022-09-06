2d0aeb3fb9e9a5647dabd562abb3eb7dea0000f7
世纪末的架构师 <1072876976@qq.com>
2022-09-06 23:47:36 +0800

    feat(项目前端): 封装上传图片组件
    1. 前端创建文件上传组件
    2. 需要绑定对话框是否显示参数、@func更新对话框是否现实参数回调函数

---
+++ blog-vue/admin/src/components/Upload/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <el-dialog
+      title="上传图片"
+      :visible="visibled"
+      width="520px"
+      :before-close="handleDialogClose"
+    >
+      <el-row>
+        <el-col :span="18" :offset="3">
+          <el-upload
+            ref="upload"
+            class="upload-container"
+            action="#"
+            drag
+            list-type="text"
+            :http-request="requestUpload"
+            :on-change="beforeUpload"
+            :auto-upload="false"
+          >
+            <i class="el-icon-upload"></i>
+            <div class="el-upload__text">
+              将文件拖到此处，或<em>点击上传</em>
+            </div>
+            <div class="el-upload__tip" slot="tip">
+              只能上传jpg/png文件，且不超过 10 MB
+            </div>
+          </el-upload>
+        </el-col>
+      </el-row>
+      <span slot="footer" class="dialog-footer">
+        <!-- @click="submitUpload" -->
+        <el-button type="primary">确定</el-button>
+        <el-button @click="handleDialogClose">取消</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Upload",
+  props: {
+    // 定义是否展示
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      visibled: this.visible,
+    };
+  },
+  watch: {
+    visible(val) {
+      //新增visble的监听变更并同步
+      this.visibled = val;
+    },
+  },
+
+  methods: {
+    // 上传前的预处理
+    beforeUpload(file) {
+      const fileType = file.name
+        .substring(file.name.lastIndexOf(".") + 1)
+        .toLowerCase();
+      const isLtSize = file.size / 1024 / 1024 < 10;
+      if (["png", "jpeg", "jpg", "gif"].indexOf(fileType) === -1) {
+        this.$message.error("上传图片只能是 JPG、PNG、GIF格式!");
+        return false;
+      }
+      if (!isLtSize) {
+        this.$message.error(`上传图片大小不能超过 10 MB!`);
+        return false;
+      }
+      return true;
+    },
+    // 关闭对话框
+    handleDialogClose() {
+      this.visibled = !this.visibled;
+      //组件内变更后向外部发送事件通知
+      this.$emit("func", this.visibled);
+    },
+    // 覆盖默认的上传行为
+    requestUpload() {},
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
