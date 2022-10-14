c3ccf7b076bf7f06e97136d5e089a2866478af55
世纪末的架构师 <1072876976@qq.com>
2022-10-14 22:13:08 +0800

    feat(前端返回数据层): 删除前端返回数据层中的Swagger对象配置
    1. 删除社交信息返回对象的Swagger配置
    2. 删除用户其他信息返回对象的Swagger配置
    3. 删除网站基本信息返回对象的Swagger配置

--- blog-springboot/src/main/java/com/zrkizzy/blog/vo/param/WebsiteVO.java
+++ blog-springboot/src/main/java/com/zrkizzy/blog/vo/param/WebsiteVO.java
@@ -1,7 +1,5 @@
 package com.zrkizzy.blog.vo.param;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -13,32 +11,41 @@ import lombok.experimental.Accessors;
  */
 @Data
 @Accessors(chain = true)
-@ApiModel(value = "网站设置数据传输对象", description = "")
 public class WebsiteVO {
-    @ApiModelProperty(value = "主键")
+    /**
+     * 主键
+     */
     private Integer id;
-
-    @ApiModelProperty(value = "网站头像")
+    /**
+     * 网站头像
+     */
     private String logo;
-
-    @ApiModelProperty(value = "网站名称")
+    /**
+     * 网站名称
+     */
     private String name;
-
-    @ApiModelProperty(value = "网站作者")
+    /**
+     * 网站作者
+     */
     private String author;
-
-    @ApiModelProperty(value = "网站简介")
+    /**
+     * 网站简介
+     */
     private String introduction;
-
-    @ApiModelProperty(value = "网站创建日期")
+    /**
+     * 网站创建日期
+     */
     private String createDate;
-
-    @ApiModelProperty(value = "公告")
+    /**
+     * 公告
+     */
     private String publish;
-
-    @ApiModelProperty(value = "座右铭")
+    /**
+     * 座右铭
+     */
     private String motto;
-
-    @ApiModelProperty(value = "备案号")
+    /**
+     * 备案号
+     */
     private String record;
 }
